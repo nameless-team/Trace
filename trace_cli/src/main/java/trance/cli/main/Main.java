17d83ab89b223b1d46025ef6dc22db0d9fb4b402
cujamin <chbnjamin@gmail.com>
2019-05-01 15:11:31 +0800

    增加cli模块

---
+++ trace_cli/src/main/java/trance/cli/main/Main.java
@@ -0,0 +1,13 @@
+package trance.cli.main;
+
+/**
+ * @Author: cujamin
+ * @ProjectName: TraceR
+ * @Date: 2019/5/1 3:06 PM
+ * @Description: ${description}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
